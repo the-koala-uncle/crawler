9b376caa46a7551043e82c35a123f2d4da2d3fcf
the-koala-uncle <1433289097@qq.com>
2017-07-20 20:20:11 +0800

    Update information of  good-users.py

--- Jianshu/information of  good-users.py
+++ Jianshu/information of  good-users.py
@@ -5,3 +5,18 @@ os.chdir(os.getcwd())
 def write():
     with open('Information.txt','a') as f:
         f.write(information+'\r\n')
+#headers={        
+got=set()
+#打印一个
+url='http://www.jianshu.com/u/03c9cd05c76d'
+res=requests.get(url,headers=headers)
+soup=beatifulsoup(res.txt,'lxml')
+ids=soup.find('a',class_='name')
+ids_name=ids.get_text()
+#ids_url=ids['herf'
+print(rds)#3
+print(rds)#3
+print(rds)#3
+#3    <a class="name" href="/u/03c9cd05c76d">考拉丶大叔</a>
+more=soup.find('div',class_='info')
+print(more)
