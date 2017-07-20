bdfce6fc52e009223042cc045558471bb2d40bf3
the-koala-uncle <1433289097@qq.com>
2017-07-20 21:42:33 +0800

    Update information of  good-users.py

--- Jianshu/information of  good-users.py
+++ Jianshu/information of  good-users.py
@@ -1,22 +1,26 @@
 import requests,os
-from bs4 import BeatifulSoup
+from bs4 import BeautifulSoup
 
 os.chdir(os.getcwd())
-def write():
+def write(TXT):
     with open('Information.txt','a') as f:
-        f.write(information+'\r\n')
-#headers={        
+        f.write(TXT+'\r\n')
+agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+headers = {
+    'User-Agent': agent
+}      
 got=set()
+number=1
 #打印一个
 url='http://www.jianshu.com/u/03c9cd05c76d'
 res=requests.get(url,headers=headers)
-soup=beatifulsoup(res.txt,'lxml')
+soup=BeautifulSoup(res.text,'lxml')
 ids=soup.find('a',class_='name')
 ids_name=ids.get_text()
-ids_url=ids['herf']
-print(rds)#3
-print(rds)#3
-print(rds)#3
-#3    <a class="name" href="/u/03c9cd05c76d">考拉丶大叔</a>
-more=soup.find('div',class_='info')
-print(more)
+ids_url='http://www.jianshu.com'+ids['href']
+morenum=soup.find('div',class_='info').find_all('p')
+
+moreurl=soup.find('div',class_='info').find('a')['href']
+
+information='（{}）{} ，关注：{} ，粉丝：{} ，收获喜欢：{} ，文章：{} ，总字数：{} ，链接：{}'.format(number,ids_name,morenum[0].get_text(),morenum[1].get_text(),morenum[4].get_text(),morenum[2].get_text(),morenum[3].get_text(),ids_url)
+write(information)
